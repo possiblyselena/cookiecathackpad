955b97e9c63143153bf0088e82c915cf86a6da9c
Selena <149027636+possiblyselena@users.noreply.github.com>
2025-06-11 22:12:19 -0400

    Create main.py
    firmware!

---
+++ firmware/main.py
@@ -0,0 +1,22 @@
+import board
+
+# These are imports from the kmk library
+from kmk.kmk_keyboard import KMKKeyboard
+from kmk.scanners.keypad import KeysScanner
+from kmk.keys import KC
+
+keyboard = KMKKeyboard()
+
+PINS = [board.D0, board.D1, board.D2, board.D3, board.D4, board.D5, board.D6, board.D7]
+
+keyboard.matrix = KeysScanner(
+    pins=PINS,
+    value_when_pressed=False,
+)
+
+keyboard.keymap = [
+    [KC.V, KC.C, KC.LFCTRL, KC.AT, KC.Y, KC.ENTER,]
+]
+
+if __name__ == '__main__':
+    keyboard.go()
